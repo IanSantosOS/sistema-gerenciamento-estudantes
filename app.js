fceffcc7eaf97b83f09c9c9ae34f175021f89d6d
Ian Santos <iann.silva2006@gmail.com>
2024-04-02 16:31:28 -0300

    update app.js

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
 const session = require('express-session');
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', 'views/pages');
-app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views', 'pages'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
-  secret: 'gerenciamento-estudantes',
-  resave: false,
-  saveUninitialized: false,
-  cookie: { secure: false }
+    secret: 'gerenciamento-estudantes',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false }
 }));
 
 // Rotas Aqui
 
+app.use((err, _req, res, _next) => {
+    console.error(err.stack);
+    res.status(500).send('Erro interno do servidor');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`\n\x1b[43;1m Funcionou!!! \x1b[0m Servidor está rodando na porta: ${PORT}\x1b[0m\n`);
+    console.log(`\n\x1b[43;1m Funcionou!!! \x1b[0m Servidor está rodando na porta: ${PORT}\x1b[0m\n`);
 });
