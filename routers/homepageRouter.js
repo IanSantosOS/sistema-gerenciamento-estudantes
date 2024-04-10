3caab9ea330cf364a8eb6e5c95c85a3110f3b3b1
Gabriel <grc3@aluno.ifal.edu.br>
2024-04-10 17:11:04 -0300

    Co-authored-by: <IanSantosOS@users.noreply.github.com>
    Update login and create home page

--- routers/homepageRouter.js
+++ routers/homepageRouter.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    res.redirect('/homepage');
+});
+
+router.get('/homepage', (req, res) => {
     res.render('homepage');
 });
 
