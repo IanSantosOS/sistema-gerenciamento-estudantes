7ea2f7c8884b54e9bc55e85bd43eabe1de5e8b7c
Ian Santos <iann.silva2006@gmail.com>
2024-07-06 22:41:30 -0300

    mais rotas

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -1,26 +1,53 @@
 const express = require('express');
 const router = express.Router();
 
-// PÁGINAS PRINCIPAIS
+const AlunoController = require('../controllers/AlunoController');
 
-router.get('/', (req, res) => {});
+// --------------- PÁGINAS PRINCIPAIS ---------------
 
-router.get('/login', (req, res) => {});
+router.post('/login', (req, res) => {
+    res.status(400).send({ msg: '* Usuário, e-mail ou senha inválidos!' });
+    res.end()
+});
 
-router.get('/homepage', (req, res) => {});
+router.post('/logout', (req, res) => {
+    if (!req.session?.username) return;
 
-// ADMIN
+    req.session.destroy((err) => {
+        if (err) {
+            console.log(err)
+            res.status(500).render('erro', {
+                contato: true,
+                titulo: '500: Erro interno do servidor',
+                msg: 'Aconteceu um erro interno dentro do nosso servidor ao tentar sair da conta.'
+            });
+        } else {
+            res.redirect('/login');
+        }
+    });
+});
+
+// --------------------- ADMIN ---------------------
 
 // router.get('/admin/cadastro', (req, res) => {
 //     // res.render('');
 // });
 
-// ALUNOS
+// --------------------- ALUNOS ---------------------
 
-router.get('/alunos/lista', (req, res) => {});
+router.get('/alunos/lista', async (req, res) => {
+    const { nome } = req.query;
+    return await AlunoController.searchByName(nome)
+});
 
-router.get('/alunos/cadastro', (req, res) => {});
+router.post('/alunos/cadastro', (req, res) => { });
 
-router.get('/alunos/atualizar', (req, res) => {});
+router.put('/alunos/atualizar/:id', (req, res) => { });
+
+router.delete('/alunos/delete/:id', (req, res) => { });
+
+router.get('/alunos/avaliacao', (req, res) => { });
+
+router.get('/alunos/frequencia', (req, res) => { });
 
 module.exports = router;
--- routers/pagesRouter.js
+++ routers/pagesRouter.js
@@ -1,45 +1,55 @@
 const express = require('express');
 const router = express.Router();
 
-// PÁGINAS PRINCIPAIS
+// --------------- PÁGINAS PRINCIPAIS ---------------
 
-router.get('/', (req, res) => {
+router.get('/', (_req, res) => {
     res.redirect('/homepage');
 });
 
-router.get('/login', (req, res) => {
+router.get('/login/', (_req, res) => {
     res.render('login');
 });
 
-router.get('/homepage', (req, res) => {
+router.get('/homepage/', (_req, res) => {
     res.render('homepage', { username: 'user', email: 'user@admin.com' }); // apenas teste, será alterado no futuro
 });
 
-// ADMIN
+// --------------------- ADMIN ---------------------
 
 // router.get('/admin/cadastro', (req, res) => {
 //     // res.render('');
 // });
 
-// ALUNOS
+// --------------------- ALUNOS ---------------------
 
-router.get('/alunos/lista', (req, res) => {
+router.get('/alunos/lista/', (req, res) => {
+    const { nome } = req.query;
+    res.render('lista-alunos');
+});
+
+router.get('/alunos/cadastro/', (req, res) => {
+    res.render('cadastro-atualizar', { username: 'user', email: 'user@admin.com', titulo: 'cadastrar aluno', script: '/js/cadastro-aluno.js' }); // apenas teste, será alterado no futuro
+});
+
+router.get('/alunos/atualizar/', (req, res) => {
+    const { id } = req.query;
+    if (id === undefined) { // mudar para uma função que pesquisa esse id em específico
+        res.render('erro', {
+            contato: true,
+            titulo: 'Aluno não encontrado',
+            msg: 'Não foi encontrado nenhum aluno com esse id para ser atualizado. Certifique-se de que o id se encontra na lista de alunos'
+        });
+    } else {
+        res.render('cadastro-atualizar', { username: 'user', email: 'user@admin.com', titulo: 'atualizar aluno', script: '/js/atualizar-aluno.js' }); // apenas teste, será alterado no futuro
+    }
+});
+
+router.get('/alunos/avaliacao/', (req, res) => {
     // res.render('');
 });
 
-router.get('/alunos/cadastro', (req, res) => {
-    res.render('cadastro');
-});
-
-router.get('/alunos/atualizar', (req, res) => {
-    // res.render('');
-});
-
-router.get('/alunos/avaliacao', (req, res) => {
-    // res.render('');
-});
-
-router.get('/alunos/frequencia', (req, res) => {
+router.get('/alunos/frequencia/', (req, res) => {
     // res.render('');
 });
 
