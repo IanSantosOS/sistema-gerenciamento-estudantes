85f51e954d2b41fc08bee47fb7041b1877c1fb77
Sofia Laura <slrm1@aluno.ifal.edu.br>
2024-07-08 10:44:53 -0300

    frontend

---
+++ public/js/login.js
@@ -0,0 +1,30 @@
+const form_login = document.querySelector('#form-login');
+const username_email = document.querySelector('#username-email');
+const password = document.querySelector('#password');
+const error = document.querySelector('#error');
+
+form_login.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const login = {
+        username_email: username_email.value,
+        password: password.value
+    }
+
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(login),
+        headers: { 'Content-Type': 'application/json' }
+    }
+
+    fetch('/api/login', options)
+        .then(res => res.json())
+        .catch(err => err.message = "")
+        .then(data => {
+            if (data.msg) {
+                error.textContent = data.msg;
+            } else {
+                window.location.replace('/homepage/');
+            }
+        });
+});
